27961176081fc6574327131bc51a3ae4af6efbb5
Nemanja <nemanja.gavrilovic1995@gmail.com>
2019-04-16 01:10:54 +0200

    Main page and entity info

--- app/src/main/java/com/projekat/pma/NewsInfo.java
+++ app/src/main/java/com/projekat/pma/NewsInfo.java
@@ -3,22 +3,14 @@ package com.projekat.pma;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.Toast;
 
-import com.projekat.pma.model.News;
-
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
+public class NewsInfo extends AppCompatActivity {
 
     private DrawerLayout dl;
     private ActionBarDrawerToggle t;
@@ -27,9 +19,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_news_info);
 
-        dl = (DrawerLayout)findViewById(R.id.activity_main);
+        dl = (DrawerLayout)findViewById(R.id.news_info);
         t = new ActionBarDrawerToggle(this, dl,R.string.Open, R.string.Close);
 
         dl.addDrawerListener(t);
@@ -45,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 switch(id)
                 {
                     case R.id.home:
-                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        Intent intent = new Intent(NewsInfo.this, MainActivity.class);
                         startActivity(intent);
 //                        NewsInfoFragment newsInfoFragment = new NewsInfoFragment();
 //                        FragmentManager manager = getSupportFragmentManager();
@@ -54,43 +46,17 @@ public class MainActivity extends AppCompatActivity {
 //                                .commit();
                         return true;
                     case R.id.settings:
-                        Toast.makeText(MainActivity.this, "Settings",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(NewsInfo.this, "Settings",Toast.LENGTH_SHORT).show();
                         return true;
                     case R.id.contact:
-                        Toast.makeText(MainActivity.this, "Contact",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(NewsInfo.this, "Contact",Toast.LENGTH_SHORT).show();
                         return true;
                     default:
                         return true;
                 }
             }
         });
-
-        News news1 = new News("Title 1","Text 1");
-        News news2 = new News("Title 2","Text 2");
-        News news3 = new News("Title 3","Text 3");
-        News news4 = new News("Title 4","Text 4");
-        News news5 = new News("Title 5","Text 5");
-
-        ArrayList<News> news = new ArrayList<>();
-        news.add(news1);
-        news.add(news2);
-        news.add(news3);
-        news.add(news4);
-        news.add(news5);
-
-        NewsListAdapter adapter = new NewsListAdapter(this,R.layout.adapter_view_layout,news);
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this, NewsInfo.class);
-                startActivity(intent);
-            }
-        });
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -99,5 +65,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
